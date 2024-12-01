f902ab8e05341e71c6d62a039997aee3152e8766
Vishal <vishal.reply@gmail.com>
2024-12-02 01:11:02 +0530

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -9,6 +9,7 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "norg"
       -- add more arguments for adding more treesitter parsers
     },
   },
