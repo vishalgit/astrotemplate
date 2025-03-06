3cf6b76ef90499922f889c087734405e3fe6094f
Vishal <vishal.reply@gmail.com>
2025-03-07 01:54:32 +0530

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,43 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects", -- Ensure textobjects are installed
+  },
   opts = {
     ensure_installed = {
       "lua",
       "vim",
       "norg"
-      -- add more arguments for adding more treesitter parsers
+      -- Add more languages if needed
+    },
+    textobjects = { -- Enable textobjects module
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump to textobject
+        keymaps = {
+          ["af"] = "@function.outer", -- Around function
+          ["if"] = "@function.inner", -- Inside function
+          ["ac"] = "@class.outer", -- Around class
+          ["ic"] = "@class.inner", -- Inside class
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- Jump in jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]c"] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[c"] = "@class.outer",
+        },
+      },
     },
   },
 }
